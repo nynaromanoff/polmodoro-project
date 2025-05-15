2e4840ed676bec57c995ce83d4b47d51263d18c2
Jessica Santos <jel_008@hotmail.com>
2025-05-15 12:51:16 -0300

    Adding Componets Logo, Menu and Edit Componet Heading

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,14 @@
+import type React from "react"
+import styles from './Logo.module.css'
+import { TimerIcon } from "lucide-react"
+
+export  function Logo () {
+     return (
+    <div className={styles.logo}>
+      <a className={styles.logoLink} href='#'>
+        <TimerIcon/>
+        <span>Chronos</span>
+      </a>
+    </div>
+  );
+}
